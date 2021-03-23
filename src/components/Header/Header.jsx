d8942d0a3ebda5eb8764f996e3357c3bbb46acf5
Mykhailo Rybak <66210950+Mykhailo-Rybak@users.noreply.github.com>
2021-03-23 15:10:24 +0200

    Fix logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import {NavLink} from "react-router-dom";
-import logo from '../../img/logo.svg'
+import logo from '../../assets/images/logo.svg'
 
 const Header = (props) => {
     return <header className='header'>
--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import preloader from "../../../assets/images/preloader.svg";
 import '../../../App.css'
+
 let Preloader = () => {
     return <div className='preloader'>
         <img src={preloader} />
